0fa1433cd8dfe383293069765b704b1271b81832
Dylan <mccarteydylan@gmail.com>
2024-12-23 15:26:37 -0600

    updated server.js

--- src/server.js
+++ src/server.js
@@ -7,4 +7,14 @@ const app = require(path.resolve(
 ));
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations);
+    app.listen(PORT, listener);
+  })
+  .catch((error) => {
+    console.error(error);
+    knex.destroy();
+  });
